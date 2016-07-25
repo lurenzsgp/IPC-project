0ff61410d11dbc79df2ca30ad4b1139852c53867
lorenzo cazzoli <lurenz.sgp@gmail.com>
2016-07-25 12:02:00 +0200

    fatta il parser per i file del livello. Problemi nell'esecuzione della funzione goal letta dal file

--- js/editor.js
+++ js/editor.js
@@ -1,7 +1,15 @@
 var editor = (function() {
     // varaibles
+    var symbols = {
+        'begin_line':'#BEGIN_EDITABLE#',
+        'end_line':'#END_EDITABLE#',
+        'start_goal_function':'#START_OF_GOAL_FUNCTION#',
+        'end_goal_function':'#END_OF_GOAL_FUNCTION#'
+    };
     var cm = {};
     var editableLines = [];
+    // mi salva la funzione al momento dell'iniliziazione di editor
+    var goalFunction = new Function();
 
     var initialize = function() {
         cm = CodeMirror.fromTextArea(document.getElementById("code"), {
@@ -13,7 +21,8 @@ var editor = (function() {
     }
 
     var loadCode = function (lvl) {
-        $.get( "lvl/lvl" + lvl + ".js", function( data ) {
+        $.get( "lvl/lvl" + lvl + ".jsx", function( data ) {
+          data = preprocessor(data);
           cm.setValue(data);
 
           cm.on('beforeChange',function(cm,change) {
@@ -21,8 +30,6 @@ var editor = (function() {
                   change.cancel();
               }
           });
-
-          editableLines = [2,3,4];
 
           cm.eachLine(function (line) {
              var i = cm.getLineNumber(line);
@@ -32,10 +39,67 @@ var editor = (function() {
           });
           cm.refresh();
         }, "text");
+
+        return goalFunction;
+    }
+
+    // preprocesses code,determines the location
+    // of editable lines, loads goal function
+    var preprocessor = function (code) {
+        editableLines = [];
+        var goalString = "";
+        var lineArray = code.split("\n");
+        var inEditableBlock = false;
+        var inGoalFunctionBlock = false;
+
+        for (var i = 0; i < lineArray.length; i++) {
+            var currentLine = lineArray[i];
+
+            /* splice e i-- servono a eliminare dal codice i symboli aggiunti per riconoscere la sintassi */
+
+            // process editable lines and sections
+            if (currentLine.indexOf(symbols.begin_line) === 0) {
+                lineArray.splice(i,1);
+                i--;
+                inEditableBlock = true;
+            } else if (currentLine.indexOf(symbols.end_line) === 0) {
+                lineArray.splice(i,1);
+                i--;
+                inEditableBlock = false;
+            }
+            // process start of GoalFunction()
+              else if (currentLine.indexOf(symbols.start_goal_function) === 0) {
+                lineArray.splice(i,1);
+                inGoalFunctionBlock = true;
+                i--;
+            }
+            // process end of GoalFunction()
+              else if (currentLine.indexOf(symbols.end_goal_function) === 0) {
+                lineArray.splice(i,1);
+                inGoalFunctionBlock = false;
+                i--;
+            }
+            // everything else
+              else {
+                if (inEditableBlock) {
+                    editableLines.push(i);
+                }
+                // save goalFunction() code
+                if (inGoalFunctionBlock) {
+                    goalString += currentLine;
+                    lineArray.splice(i,1);
+                    i--;
+                }
+            }
+            goalFunction = new Function(goalString);
+        }
+
+        return lineArray.join("\n");
     }
 
     return {
         initialize: initialize,
-        loadCode: loadCode
+        loadCode: loadCode,
+        goalFunction : goalFunction
     }
 })();
--- lvl/lvl1.jsx
+++ lvl/lvl1.jsx
@@ -1,6 +1,11 @@
 // Constructor for an Anti Missile Battery
 function AntiMissileBattery( x, y ) {
+#BEGIN_EDITABLE#
   this.x = x;
   this.y = y;
   this.missilesLeft = 10;
+#END_EDITABLE#
 }
+#START_OF_GOAL_FUNCTION#
+console.log("goal function del livello di prova");
+#END_OF_GOAL_FUNCTION#
