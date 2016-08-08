ffeb1eb2d7c34b423a9eb26f0d152f7c499dd17e
ManfrediG <manfredi.giordano@me.com>
2016-08-08 18:17:47 +0200

    Aggiunto 1 addon CodeMirror
    Autocomplete (Javascript Hint)

--- js/app.js
+++ js/app.js
@@ -6,3 +6,35 @@ $(document).ready(function () {
     missileCommand.initialize();
     missileCommand.setupListeners();
 });
+
+// addon Autocomplete CodeMirror
+if (typeof Promise !== undefined) {
+  var comp = [
+    ["here", "hither"],
+    ["asynchronous", "nonsynchronous"],
+    ["completion", "achievement", "conclusion", "culmination", "expirations"],
+    ["hinting", "advive", "broach", "imply"],
+    ["function","action"],
+    ["provide", "add", "bring", "give"],
+    ["synonyms", "equivalents"],
+    ["words", "token"],
+    ["each", "every"],
+  ]
+
+  function synonyms(cm, option) {
+    return new Promise(function(accept) {
+      setTimeout(function() {
+        var cursor = cm.getCursor(), line = cm.getLine(cursor.line)
+        var start = cursor.ch, end = cursor.ch
+        while (start && /\w/.test(line.charAt(start - 1))) --start
+        while (end < line.length && /\w/.test(line.charAt(end))) ++end
+        var word = line.slice(start, end).toLowerCase()
+        for (var i = 0; i < comp.length; i++) if (comp[i].indexOf(word) != -1)
+          return accept({list: comp[i],
+                         from: CodeMirror.Pos(cursor.line, start),
+                         to: CodeMirror.Pos(cursor.line, end)})
+        return accept(null)
+      }, 100)
+    })
+  }
+}
--- js/editor.js
+++ js/editor.js
@@ -14,6 +14,7 @@ var Editor = function () {
 	    matchBrackets: true,
 		showTrailingSpace: true,
 	    autoCloseBrackets: true,
+		extraKeys: {"Ctrl-Space": "autocomplete"},
         mode: "javascript"
     });
 }
