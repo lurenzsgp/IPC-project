d843e7fcb291e23b4dd7684847ea64bd8d2c60de
Alice <llice@hotmail.it>
2016-08-18 17:45:01 +0200

    prove express

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile('\index.html');
+  res.send('\index.html');
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,15 @@
-var express = require('express');
-var app = express();
+var express    =    require('express');
+var app        =    express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+/*
+require('./routes/main')(app);				//router file
+app.set('views',__dirname + '/views');		//where html file are placed
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+*/
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+require('./routes/index')(app);
+
+var server = app.listen(3000,function(){
+	console.log("Express is running on port 3000");
 });
