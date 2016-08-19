ea38693386f40789d6e5d3c28dea340023a68a59
Alice <llice@hotmail.it>
2016-08-19 17:22:05 +0200

    sistemato express e creata pagina 404

--- server.js
+++ server.js
@@ -1,15 +1,34 @@
-var express    =    require('express');
-var app        =    express();
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
 
-/*
-require('./routes/main')(app);				//router file
-app.set('views',__dirname + '/views');		//where html file are placed
-app.set('view engine', 'ejs');
-app.engine('html', require('ejs').renderFile);
-*/
+app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static('assets));
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
 
-require('./routes/index')(app);
+app.use(bodyParser.json());
+app.engine('.html', require('ejs').__express);
+app.set('views', __dirname);
+app.set('view engine', 'html');
+
+
+//require('./routes/index')(app);
 
 var server = app.listen(3000,function(){
 	console.log("Express is running on port 3000");
+});
+
+app.get('/index', function(req, res){	
+	res.render('index');
+});
+
+app.get('/', function(req, res){
+	res.render('login');
+});
+
+app.get('/*', function(req, res){
+	res.render('404');
 });
