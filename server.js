e2b3d284fe48f304c40852f9895c410f90186967
ManfrediG <manfredi.giordano@me.com>
2016-08-24 12:27:41 +0200

    Aggiunti i moduli Knex e Bookshelf
    Modificato server.js per includere i nuovi moduli

--- server.js
+++ server.js
@@ -1,31 +1,41 @@
-var express = require('express');
-var app = express();
-var path = require('path');
-var bodyParser = require('body-parser');
 var dbConfig;
-
 try {
     dbConfig = require('./config/db.conf.js');
-} catch(e) {
+} catch(err) {
 	console.log('Startup failed. No DB config file found.');
 	return false;
 }
+
+var express = require('express'),
+	path = require('path'),
+	bodyParser = require('body-parser'),
+	knex = require('knex')({
+		client: 'mysql',
+		connection: dbConfig
+	});
+
+var crypto;
+try {
+	crypto = require('crypto');
+} catch (err) {
+	console.log('Crypto support is disabled in Node.');
+}
+
+var app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 //app.use(express.static('assets));
 app.use(bodyParser.urlencoded({
 	extended: true
 }));
-
 app.use(bodyParser.json());
 app.engine('.html', require('ejs').__express);
 app.set('views', __dirname);
 app.set('view engine', 'html');
 
-
 //require('./routes/index')(app);
 
-var server = app.listen(3000,function(){
+var server = app.listen(3000, function(){
 	console.log("Express is running on port 3000");
 });
 
